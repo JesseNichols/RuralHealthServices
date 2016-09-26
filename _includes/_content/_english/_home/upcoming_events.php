eb72e64945ffb73c4f0650291646999a34a8935c
Jesse Nichols <jesse@happyninja.io>
2016-09-26 19:20:36 -0400

    Multiple Updates

--- _includes/_content/_english/_home/upcoming_events.php
+++ _includes/_content/_english/_home/upcoming_events.php
@@ -9,60 +9,13 @@
                     <th>Date</th>
                 </tr>
                 <tr>
-                    <td>Teen Clinic</td>
-                    <td>MJW</td>
-                    <td>June 27</td>
+                    <td>Mobile Unit Coming Soon!</td>
+                    <td>TBA</td>
+                    <td>TBA</td>
                 </tr>
                 <tr>
-                    <td colspan="3">TEEN CLINIC: Every 2nd Wednesday <br>
-4:00 pm -7:00 pm <br>
-Individuals &amp; Groups <br>
-Contact: Jackie Lawrence @(803) 508-7121</td>
+                    <td colspan="3">Mobile Unit Coming Soon!</td>
                 </tr>
-                <tr>
-                    <td>Teen Clinic</td>
-                    <td>CCPC</td>
-                    <td>July 13</td>
-                </tr>
-                <tr>
-                    <td colspan="3">TEEN CLINIC: Every 2nd Wednesday <br>
-4:00 pm -7:00 pm <br>
-Individuals &amp; Groups <br>
-Contact: Jackie Lawrence @(803) 508-7121</td>
-                </tr>
-                <tr>
-                    <td>Look Good Feel Better</td>
-                    <td>CCPC</td>
-                    <td>July 18</td>
-                </tr>
-                <tr>
-                    <td colspan="3">Look Good Feel Better: Every 3rd Monday <br>
-1:00 pm - 3:00 pm <br>
-This is a FREE Program from the American Cancer Society designed for women dealing with hair loss and skin changes from chemotherapy &amp; radiation. Participants learn specific techniques to help make the most of their appearance while undergoing treatment. All participants will take home a makeup package valued at $200! <br>
-*ADVANCE REGISTRATION IS REQUIRED <br>
-Contact: Gail B. Diggs @(803) 380-7013)</td>
-                </tr>
-                <tr>
-                    <td>Teen Clinic</td>
-                    <td>MJW</td>
-                    <td>July 28</td>
-                </tr>
-                <tr>
-                    <td colspan="3">TEEN CLINIC: Every 2nd Wednesday <br>
-4:00 pm -7:00 pm <br>
-Individuals &amp; Groups <br>
-Contact: Jackie Lawrence @(803) 508-7121</td>
-                </tr>
-                <!-- <tr>
-                    <td>WIC</td>
-                    <td>CCPC</td>
-                    <td>May 13</td>
-                </tr>
-                <tr>
-                    <td colspan="3">WIC: Every Friday <br>
-                8:30 am – 4:30 pm <br>
-                Please schedule with OB/GYN or Pediatric Front Desk staff or call (803) 380-7000</td>
-                </tr> -->
             </tbody>
         </table>
 </div>
